5b96361eb74d781f5341b6730e45a7adeb31fe84
Xhq <xuhaoqing2013@hotmail.com>
2023-11-28 12:34:59 +0800

    feat(css): 添加主题颜色

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import "./css/tailwindcss.css"
 
 import { createApp } from 'vue'
 import App from './App.vue'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: 'var(--color-text)',
+        textDark: 'var(--color-text-dark)',
+        subtext: 'var(--color-text-sub)',
+        subtextDark: 'var(--color-text-sub-dark)',
+        bg: 'var(--color-background)',
+        bgDart: 'var(--color-background-dark)',
+        subbg: 'var(--color-background-sub)',
+        subbgDark: 'var(--color-background-sub-dark)',
+      }
+    },
   },
   plugins: [],
-}
-
+  darkMode: "class",
+};
