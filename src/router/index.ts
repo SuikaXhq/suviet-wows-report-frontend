03f408e2b0c8a0235a317913cf95dcac23172e1b
xuhao <xuhaoqing2013@hotmail.com>
2023-12-04 21:50:58 +0800

    feat(router): 添加GroupList route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,15 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import GroupList from '../views/GroupList.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import GroupList from '../views/GroupList.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/group',
-      name: 'groupList',
-      component: GroupList
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/group',
+            name: 'groupList',
+            component: GroupList,
+        },
+    ],
+});
 
-export default router
+export default router;
