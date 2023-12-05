e48f31528c8b4074a348521fe34d286f700b7c3f
Xhq <xuhaoqing2013@hotmail.com>
2023-12-06 00:51:21 +0800

    misc(css): 配置tailwind编译

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        text: 'var(--color-text)',
-        textDark: 'var(--color-text-dark)',
-        subtext: 'var(--color-text-sub)',
-        subtextDark: 'var(--color-text-sub-dark)',
-        bg: 'var(--color-background)',
-        bgDark: 'var(--color-background-dark)',
-        subbg: 'var(--color-background-sub)',
-        subbgDark: 'var(--color-background-sub-dark)',
-      }
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
-  darkMode: "class",
+    plugins: [],
+    darkMode: 'class',
 };
