cc26988c3142e619893f628644f1c006808a1f82
Xhq <xuhaoqing2013@hotmail.com>
2023-12-06 00:52:26 +0800

    feat(router): 添加几个占位router用于测试

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import GroupList from '../views/GroupList.vue';
+import GroupList from '@/views/GroupList.vue';
+import BasePage from '@/components/BasePage.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
+            path: '/',
+            name: 'home',
+            component: BasePage,
+        },
+        {
             path: '/group',
             name: 'groupList',
             component: GroupList,
+        },
+        {
+            path: '/account',
+            name: 'account',
+            component: BasePage,
         },
     ],
 });
