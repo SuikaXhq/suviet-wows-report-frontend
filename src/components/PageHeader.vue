a7134cf46a0de756179ca959ca5b31f266ae9e5b
Xhq <xuhaoqing2013@hotmail.com>
2023-12-06 00:50:33 +0800

    feat(components.PageHeader): 修改样式

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,28 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+const navList = [
+    {
+        name: '主页',
+        path: 'home',
+        icon: 'eva-home',
+    },
+    {
+        name: '帐号',
+        path: 'account',
+        icon: 'eva-person',
+    },
+    {
+        name: '群组',
+        path: 'groupList',
+        icon: 'eva-people',
+    },
+];
+</script>
 <template>
     <div
-        class="flex items-center justify-around p-10 text-3xl text-gray-500 dark:text-gray-400"
+        class="flex items-center justify-around p-8 text-3xl text-gray-500 dark:text-gray-400"
     >
-        <RouterLink
-            to="/"
-            title="主页"
-            class="flex items-center justify-center transition hover:text-blue-500"
-        >
-            <i class="eva eva-home h-8 w-8 text-center"></i>
-        </RouterLink>
-        <RouterLink
-            to="/account"
-            title="帐号"
-            class="flex items-center justify-center transition hover:text-blue-500"
-        >
-            <i class="eva eva-person h-8 w-8 text-center"></i>
-        </RouterLink>
-        <RouterLink
-            to="/group"
-            title="群组"
-            class="flex items-center justify-center transition hover:text-blue-500"
-        >
-            <i class="eva eva-people h-8 w-8 text-center"></i>
-        </RouterLink>
+        <template v-for="nav in navList">
+            <RouterLink
+                :to="{ name: nav.path }"
+                :title="nav.name"
+                class="relative flex items-center justify-center transition hover:text-blue-500"
+            >
+                <i :class="`eva ${nav.icon} h-8 w-8 text-center`"></i>
+            </RouterLink>
+        </template>
     </div>
 </template>
+<style scoped>
+.router-link-active {
+    color: #fafafa;
+}
+.router-link-active::before {
+    transform: scale(1.6);
+    opacity: 1;
+}
+a::before {
+    content: '';
+    position: absolute;
+    display: block;
+    height: 100%;
+    width: 100%;
+    border-radius: 100%;
+    opacity: 0;
+    background-color: #3b82f6;
+    transition-property: all;
+    transition-duration: 300ms;
+    transform: scale(0);
+}
+</style>
