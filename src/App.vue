1458d4439b1fa8b4dd90cd296aecfafd4e17e929
Xhq <xuhaoqing2013@hotmail.com>
2023-12-05 22:07:09 +0800

    feat(App.vue): 添加PageHeader

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView } from 'vue-router';
+import PageHeader from './components/PageHeader.vue';
 </script>
 
 <template>
-  <div class="relative mx-auto max-w-4xl shadow-lg">
-    <header></header>
-    <RouterView />
-  </div>
+    <div
+        class="relative mx-auto max-w-4xl divide-y-2 divide-gray-500 bg-white text-black shadow-lg dark:divide-gray-600 dark:bg-slate-800 dark:text-white"
+    >
+        <PageHeader />
+        <RouterView />
+    </div>
 </template>
